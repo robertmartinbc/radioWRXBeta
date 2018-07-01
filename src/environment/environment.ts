a4dd423f0825cee71e8ae1cb0c03ece47f390cad
Robert Martin <robertmartinbc@shaw.ca>
2018-06-30 17:00:54 -0700

    Connected sign up for new users.

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -3,12 +3,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCHjrxeH8VUADKLhR5Csa54cqPRMDr4Pw8",
-    authDomain: "rosy-dynamics-171007.firebaseapp.com",
-    databaseURL: "https://rosy-dynamics-171007.firebaseio.com",
-    projectId: "rosy-dynamics-171007",
-    storageBucket: "rosy-dynamics-171007.appspot.com",
-    messagingSenderId: "303898922125"
+    apiKey: "AIzaSyCnJ2F67nX4-ejJHYod-OHXKkn_gIafK1A",
+    authDomain: "radiowrxbeta.firebaseapp.com",
+    databaseURL: "https://radiowrxbeta.firebaseio.com",
+    projectId: "radiowrxbeta",
+    storageBucket: "radiowrxbeta.appspot.com",
+    messagingSenderId: "510119866853"
   },
   google_web_client_id: "1092390853283-i98feg7fb1dlsm92kkcbim62855pepi8.apps.googleusercontent.com",
   facebook_app_id: 826720427470540,
